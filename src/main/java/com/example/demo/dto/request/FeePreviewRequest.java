49de9aef92ccfbd40bdb27c567c9586384b28302
Danny Fitriyana <danny.fitriyana@inmotion.co.id>
2026-08-11 16:57:59 +0700

    feat: add fee preview request dto

---
+++ src/main/java/com/example/demo/dto/request/FeePreviewRequest.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.example.demo.dto.request;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+import com.example.demo.domain.DriverPing;
+import com.example.demo.domain.EndReason;
+import com.example.demo.domain.PickupPoint;
+
+import lombok.Data;
+
+/**
+ *
+ * @author Danny Fitriyana
+ */
+
+@Data
+public class FeePreviewRequest {
+
+    private OffsetDateTime arrtivedAt;
+
+    private OffsetDateTime endedAt;
+
+    private EndReason endReason;
+
+     private PickupPoint pickupPoint;
+
+    private List<DriverPing> driverPings;
+}
