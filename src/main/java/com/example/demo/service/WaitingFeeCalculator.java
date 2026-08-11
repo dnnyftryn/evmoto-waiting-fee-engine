fb80af5ee1f02a537580d471f29dba1c88891411
Danny Fitriyana <danny.fitriyana@inmotion.co.id>
2026-08-11 17:25:40 +0700

    feat: add waiting fee calculator skeleton

---
+++ src/main/java/com/example/demo/service/WaitingFeeCalculator.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.example.demo.service;
+
+import com.example.demo.domain.FeeBreakdown;
+import com.example.demo.dto.request.FeePreviewRequest;
+
+/**
+ *
+ * @author dolphin
+ */
+public class WaitingFeeCalculator {
+
+    public FeeBreakdown calculate(FeePreviewRequest request) {
+        return null;
+    }
+}
